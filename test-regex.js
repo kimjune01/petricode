d819a209edf4a3a76d23472a7ba2bad8ff097bdf
June Kim <junekim@eav1085755m.eac.ad.ea.com>
2026-04-17 00:12:50 -0700

    Remove gemini bug-hunt scratch file

--- test-regex.js
+++
@@ -1,7 +0,0 @@
-import { buildIgnorePredicate } from "./src/filter/gitignore.js";
-const patterns = ["a/**/b", "**/b", "src/**/*.ts"];
-patterns.forEach(p => {
-  const pred = buildIgnorePredicate([p]);
-  console.log(p);
-  pred("dummy"); // dummy call
-});
